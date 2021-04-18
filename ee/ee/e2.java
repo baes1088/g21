7fe94744a85641011e26615c6096926073c4af84
beas1088 <baes1088@gmail.com>
2021-04-18 23:06:58 +0430

    cvb

---
+++ ee/ee/e2.java
@@ -0,0 +1,10 @@
+package ee;
+
+public class e2 {
+
+	public static void main (String [] args) {
+		
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ ee/ee/e5.java
@@ -0,0 +1,14 @@
+package ee;
+
+public class e5 {
+
+	public e5() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
